90f09bbc2abd0513c09ebe94f616af96dee05c6f
Frederik Creemers <frederikcreemers@gmail.com>
2018-11-04 17:10:23 +0100

    Perform spotify search, and render a link to the artist and album page.

--- src/features/PhotoEntities.js
+++ src/features/PhotoEntities.js
@@ -8,6 +8,31 @@ class PhotoEntities extends React.Component {
     }
   }
   
+  async sendSpotifyRequest() {
+    let album = this.state.musicAlbum && this.state.musicAlbum.name;
+    let group = this.state.musicGroup && this.state.musicGroup.name;
+    
+    let token = "BQD7eGIRAiF_Wlj3CgE_BaBeOO1IAQW7lO68RH-Y7-xIZe39FWVB2ApQj2dWoiaHAKJNQ7sfyl0m9LzNO_o\n";
+    let resp = await fetch(`https://api.spotify.com/v1/search?q=album:${album} artist:${group}&type=album`, {
+      method: "GET",
+      headers: {
+        "Authorization": `Bearer ${token}`
+      }
+      
+    });
+    
+    let obj = await resp.json();
+    
+    if(obj.albums && obj.albums.items.length > 0){
+      let album = obj.albums.items[0];
+      let album_url = album.external_urls.spotify;
+      let artist_url = album.artists[0].external_urls.spotify;
+      this.setState({album_url, artist_url});
+    }
+    
+    console.log(obj);
+  }
+  
   async sendKnowledgeGraphRequest() {
     let ids = this.props.entities.slice(0, 5).map((e) => e.entityId);
     let resp = await fetch("https://kgsearch.googleapis.com/v1/entities:search?key=AIzaSyAqL5fhzoTxshwa79rW0yzYFEylBxqmTGs" + ids.map((id) => {
@@ -31,7 +56,7 @@ class PhotoEntities extends React.Component {
       }
     });
     
-    this.setState({musicGroup, musicAlbum});
+    this.setState({musicGroup, musicAlbum}, this.sendSpotifyRequest);
   }
   
   componentDidMount(){
@@ -40,7 +65,10 @@ class PhotoEntities extends React.Component {
   
   
   render(){
-    return <p>Group: {this.state.musicGroup && this.state.musicGroup.name} album: {this.state.musicAlbum && this.state.musicAlbum.name}</p>
+    return <p>
+      Group: <a href={this.state.artist_url}>{this.state.musicGroup && this.state.musicGroup.name}</a>
+      album: <a href={this.state.album_url}>{this.state.musicAlbum && this.state.musicAlbum.name}</a>
+    </p>
   }
 }
 
